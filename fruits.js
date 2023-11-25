df2f855b5881dee815577b1f805f92f403237a4d
tinon1004 <tinon1004@ajou.ac.kr>
2023-11-26 05:09:03 +0900

    Feat: fruits radius

---
+++ fruits.js
@@ -0,0 +1,42 @@
+export const FRUITS = [
+    {
+        name: "샤인머스켓",
+        radius: 33/2,
+    },
+    {
+        name: "체리",
+        radius: 48/2,
+    },
+    {
+        name: "귤",
+        radius: 69/2,
+    },
+    {
+        name: "사과",
+        radius: 89/2,
+    },
+    {
+        name: "포도",
+        radius: 114/2,
+    },
+    {
+        name: "바나나",
+        radius: 142/2,
+    },
+    {
+        name: "파인애플",
+        radius: 182/2
+    },
+    {
+        name: "멜론",
+        radius: 210/2,
+    },
+    {
+        name: "수박",
+        radius: 259/2,
+    },
+
+
+
+
+];
